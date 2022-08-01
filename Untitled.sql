1a6b024deb3d0fbcbe18102c211e81651cd5a394
fortuneq <vladvolkov03@example.com>
2022-08-01 14:18:24 +0300

    added sql

---
+++ Untitled.sql
@@ -0,0 +1,6 @@
+CREATE TABLE "meet" (
+  "id" SERIAL PRIMARY KEY,
+  "user_name" varchar,
+  "created_at" timestamp,
+  "is_appoint" bool
+);
---
+++ go.mod
@@ -0,0 +1,5 @@
+module bot
+
+go 1.18
+
+require github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
